df8891708d97b161c90f893c1b8373546d8bebd1
smoke11 <bierkionline@gmail.com>
2013-04-18 00:35:13 +0200

    downloaded images now put in seperate folders for each month and year. and some fixes

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ ignorewords = { #words to ignore. check before spliting
     "48-square", #icons
 }
 main_folder = "/soup_io_images/"
+image_folder = ""
 start_time = datetime.datetime.now()
 strstarttime = str(start_time.hour)+'_'+str(start_time.minute)+"_"+str(start_time.second)
 
@@ -29,7 +30,16 @@ def log(message, colls):
             print "------------\r"
     f.close()
 
-def main(username, out_folder="/soup_io_images/", since="0"): # as base of coding, used this: http://stackoverflow.com/questions/257409/download-image-file-from-the-html-page-source-using-python
+def main(username, since="0"): # as base of coding, used this: http://stackoverflow.com/questions/257409/download-image-file-from-the-html-page-source-using-python
+    global main_folder
+    main_folder = main_folder+username+"/"
+    if not os.path.exists(main_folder):
+        try:
+            os.makedirs(main_folder)
+            log("Created folder: {0}\r".format(main_folder),False)
+        except OSError:
+            log("Can`t create folder:{0}. Probably no write rights on this disk/account or no free space.\r".format(main_folder),True)
+            image_folder=""
     url="http://"+username+".soup.io"
     nextpage=""
     page=0
@@ -40,7 +50,7 @@ def main(username, out_folder="/soup_io_images/", since="0"): # as base of codin
     lastTimeOfPost="no date yet"
     timeOfPost = "no date yet"
     #check if download from newest reposts or from specific
-    if since is not 0:
+    if since != "0":
         if since[0:5]=='date=':
             nextpage ="/?"+since
             lastTimeOfPost=since[5:]
@@ -52,7 +62,7 @@ def main(username, out_folder="/soup_io_images/", since="0"): # as base of codin
     try:
         soup = bs(urlopen(url+nextpage))
     except BaseException:
-        log("Something wrong went with: {0} \rPossibly connection shut down. Now waiting a minute in peace and then carry on.\r".format(fullUrl), True)
+        log("Something wrong went with: {0} \rPossibly connection shut down. Now waiting a minute in peace and then carry on.\r".format(url+nextpage), True)
         time.sleep(60) #wait a minute to cooldown i guess
     newusername=soup.findAll("h2")
     if len(newusername)>0:
@@ -68,7 +78,7 @@ def main(username, out_folder="/soup_io_images/", since="0"): # as base of codin
         #get url
         fullUrl=url+nextpage
         #write info
-        log("[{0}] Getting images for page: {1}/{2} (max), from: {3}, for url: {4}. (Last date: {5})\n".format(datetime.datetime.now(),page, maxPages, timeStamp, fullUrl,lastTimeOfPost), True)
+        log("[{0}] Getting images for page: {1}/{2} (max), from: {3}, for url: {4}. (Last date: {5})\r".format(datetime.datetime.now(),page, maxPages, timeStamp, fullUrl,lastTimeOfPost), True)
         #try download page source
         try:
             soup = bs(urlopen(fullUrl))
@@ -78,7 +88,7 @@ def main(username, out_folder="/soup_io_images/", since="0"): # as base of codin
             time.sleep(60) #wait a minute to cooldown i guess
             continue
         #download images
-        downloadImages(soup,parsed,out_folder, strstarttime)
+        downloadImages(soup,parsed)
         #find if there is next page
         load_more=soup.findAll(id="load_more")
         if len(load_more)>0:
@@ -98,7 +108,7 @@ def main(username, out_folder="/soup_io_images/", since="0"): # as base of codin
         lastNumberOfLinks=numberOfLinks
 
 
-def downloadImages(soup, parsed, out_folder, strstarttime):
+def downloadImages(soup, parsed):
     timeOfPost="can`t find a date"
     for image in soup.findAll("img"):
     #check image tag is broken
@@ -137,13 +147,28 @@ def downloadImages(soup, parsed, out_folder, strstarttime):
                             if len(abbrs)>=0:
                                 timeOfPost=abbrs[0].attrs[0][1]
                                 lastTimeOfPost=timeOfPost
+                                split = timeOfPost.split(" ")
+                                year = split[2]
+                                month = split[0]
+                                day = split[1]
+                                image_folder=year
+                                #making folders for specific year and month
+                                image_folder=year+"/"+month
+                                if not os.path.exists(main_folder+image_folder):
+                                    try:
+                                        os.makedirs(main_folder+image_folder)
+                                        log("Created folder for path: {0} in main folder: {1}\r".format(image_folder, main_folder),False)
+                                    except OSError:
+                                        log("Can`t create folder: {0} in main folder: {1}. Probably no write rights on this disk/account or no free space.\r".format(image_folder, main_folder),True)
+                                        image_folder=""
+
             log("Image: {0}, from: {1}\r".format(image['src'],timeOfPost),False)
 
 
         time.sleep(0.1) #for less work on servers
         filename = image["src"].split("/")[-1] #taking name of image
         parsed[2] = image["src"]
-        outpath = os.path.join(out_folder, filename)
+        outpath = os.path.join(main_folder+image_folder, filename)
         # try download image
         try:
             if image["src"].lower().startswith("http"):
@@ -158,9 +183,10 @@ def downloadImages(soup, parsed, out_folder, strstarttime):
 def _usage():
     print "Downloader for images from soup.io sites"
     print "usage: python main.py username pathForImages since/date\r"
+    print "i.e. 'python main.py smoke11'"
     print "i.e. python main.py smoke11 E:\soupioimages date=2012-04-02\r"
     print "i.e. python main.py smoke11 E:\soupioimages 296358805\n"
-    print "pathForImages - optional argument. if it`s empty it will make folder 'soup_io_images' in your script directory\n"
+    print "pathForImages - optional argument. if you leave it empty it will make folder 'soup_io_images' in your script directory\n"
     print "since/date - optional argument, you can put date in format date=YYYY-MM-DD, \r"
     print "or you can put since number - you can get it from soup.io url, when loading older post manually. \r"
     print "i.e. 'http://smoke11.soup.io/since/52838091?mode=own%3D8ee428d6102f5fcece4826e1cc4b6c3a' where wanted number is between 'since/'and '?' sign\r"
@@ -168,9 +194,16 @@ def _usage():
 
 if __name__ == "__main__":
     _usage()
+    since_date=None
     username = sys.argv[1]
-    main_folder = sys.argv[2]
-    since_date=sys.argv[3]
+    if len(sys.argv)>2:
+        main_folder = sys.argv[2]
+        if main_folder[-1]!="/":
+            main_folder+= "/"
+    else:
+        main_folder = "/soup_io_images/"
+    if len(sys.argv)>3:
+        since_date=sys.argv[3]
     if not os.path.exists(main_folder):
         try:
             os.makedirs(main_folder)
@@ -183,4 +216,8 @@ if __name__ == "__main__":
     if username==None or username=="" or username==" ":
         log("You must specify username to run this script!\r")
         exit()
-    main(username, main_folder, since_date)
+    if since_date!=None:
+        main(username, since_date)
+    else:
+        main(username)
+
